82c3d1f1fe2f9e819e6b69a6cd8f2caea3888359
Timothy Jensen <timothy.jensen.13@gmail.com>
2023-03-21 11:11:06 -0400

    stuck on Company

--- lib/company.py
+++ lib/company.py
@@ -2,9 +2,12 @@ from .freebie import Freebie
 
 class Company:
     
+    all = []
+
     def __init__(self, name, year):
         self.name = name
         self.year = year
+        Company.all.append(self)
 
     @property
     def freebies(self):
@@ -14,3 +17,11 @@ class Company:
     @property
     def devs(self):
         return [f.dev_instance.name for f in Freebie.all if f.company_instance == self]
+    
+    def give_freebie(self, item_name, value, dev):
+        return Freebie(item_name, value, dev, self)
+    
+    @classmethod
+    def oldest_company(cls):
+        sorted_companies = cls.all.sort()
+        return sorted_companies[0]
